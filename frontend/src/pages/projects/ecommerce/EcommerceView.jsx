02a7a7c1248fac8cffaaaa2bcccc4354641555b8
Anant003 <anantpanchal39@gmail.com>
2025-10-02 13:14:42 +0530

    add ecommerce component

---
+++ frontend/src/pages/projects/ecommerce/EcommerceView.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const EcommerceView = () => {
+  return (
+    <div>
+        <h1>Ecommerce Projects</h1>
+        <p>Explore various ecommerce related projects and examples.</p>
+    </div>
+  )
+}
+
+export default EcommerceView;
